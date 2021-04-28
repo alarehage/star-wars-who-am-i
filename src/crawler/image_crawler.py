f92884f1a87636ff37602c6b4c7e6ccfe0b30242
ruihan <rui_han_lim@aiap.sg>
2021-04-29 00:40:10 +0800

    updated crawler call args

--- src/crawler/image_crawler.py
+++ src/crawler/image_crawler.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 SCRIPT_PATH = Path(__file__).parent.absolute()
 
+
 def download_images(config: dict) -> None:
     """
     Function to download images from google
@@ -11,10 +12,19 @@ def download_images(config: dict) -> None:
     Args:
         config (dict): params for downloading
     """
-    
+
     for keyword in config["keywords"]:
-        google_crawler = GoogleImageCrawler(storage={"root_dir": f"project_data/images/{keyword}"})
-        google_crawler.crawl(keyword=f"{keyword} star wars", max_num=config["max_num"])
+        google_crawler = GoogleImageCrawler(
+            feeder_threads=2,
+            parser_threads=4,
+            downloader_threads=10,
+            storage={"root_dir": f"project_data/images/{keyword}"},
+        )
+        google_crawler.crawl(
+            keyword=f"{keyword} star wars",
+            max_num=config["max_num"],
+            filters={"date": ((1990, 1, 1), (2021, 12, 31))},
+        )
 
 
 if __name__ == "__main__":
