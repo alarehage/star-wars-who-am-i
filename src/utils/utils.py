fe53ffec599f7be334469581cf6e0e538b452393
ruihan <lrh1000@hotmail.com>
2021-05-18 02:22:39 +0800

    created utils script for converting images

---
+++ src/utils/utils.py
@@ -0,0 +1,13 @@
+import re
+import base64
+from PIL import Image
+from io import BytesIO
+
+
+def base64_to_pil(img_base64):
+    """
+    Convert base64 image data to PIL image
+    """
+    image_data = re.sub("^data:image/.+;base64,", "", img_base64)
+    pil_image = Image.open(BytesIO(base64.b64decode(image_data)))
+    return pil_image
