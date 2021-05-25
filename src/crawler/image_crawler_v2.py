18e2f4b2d44c94ff8ba3d8374e4644fdb3694633
ruihan <lrh1000@hotmail.com>
2021-05-26 04:58:36 +0800

    fixed pathing

--- src/crawler/image_crawler_v2.py
+++ src/crawler/image_crawler_v2.py
@@ -87,8 +87,8 @@ def persist_image(folder_path: str, url: str):
     try:
         image_file = io.BytesIO(image_content)
         image = Image.open(image_file).convert("RGB")
-        file_path = (
-            Path(folder_path) / hashlib.sha1(image_content).hexdigest()[:10] / ".jpg"
+        file_path = Path(folder_path) / (
+            hashlib.sha1(image_content).hexdigest()[:10] + ".jpg"
         )
         with open(file_path, "wb") as f:
             image.save(f, "JPEG", quality=85)
